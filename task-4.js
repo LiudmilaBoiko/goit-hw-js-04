2605e0db34ff00d855ff3adbcda4c1ca615cda9a
Lyudmyla Boiko <itsforyou.com.ua@gmail.com>
2020-09-09 21:30:34 +0300

    final

--- task-4.js
+++ task-4.js
@@ -4,29 +4,36 @@ const account = {
   discount: 0.1,
   orders: ["order-1", "order-2", "order-3"],
   changeDiscount(value) {
-    discount = value; // Write code in this line
+    this.discount = value; // Write code in this line
   },
   showOrders() {
-    return orders; // Write code in this line
+    return this.orders; // Write code in this line
   },
   addOrder(cost, order) {
-    balance -= cost; // Write code in this line
-    orders.push(order); // Write code in this line
+    this.balance -= cost; // Write code in this line
+    this.orders.push(order); // Write code in this line
   },
 };
-
 const copyAccount = Object.assign({}, account);
 copyAccount.orders = [...account.orders];
 // копируем для автотестов ссылочные типы
 
+/*
 account.changeDiscount(0.15);
-console.log(account.discount); // 0.15
+//console.log(account.discount); // 0.15
 
 //console.log(account.showOrders());
 // ['order-1', 'order-2', 'order-3']
 
-account.addOrder(5000, "order-4");
+account.addOrder(5000, 'order-4');
 //console.log(account.balance); // 19000
 
 //console.log(account.showOrders());
 // ['order-1', 'order-2', 'order-3', 'order-4']
+*/
+
+//Задача 4 - 4
+//this в методах объекта
+//Расставь отсутствующие this в методах объекта account.
+
+//В комментариях показаны операции с объектом и ожидаемые результаты.
